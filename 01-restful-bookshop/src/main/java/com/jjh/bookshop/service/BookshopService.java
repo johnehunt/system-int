30fa474cdabbe4f82e9d9b63b97ba927f8b85a0a
John Hunt <john.hunt10@gmail.com>
2020-12-17 16:13:21 +0000

    Added ability to turn off logging

--- 01-restful-bookshop/src/main/java/com/jjh/bookshop/service/BookshopService.java
+++ 01-restful-bookshop/src/main/java/com/jjh/bookshop/service/BookshopService.java
@@ -1,5 +1,6 @@
 package com.jjh.bookshop.service;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookshopService {
     public static void main(String[] args) {
         System.out.println("Starting the web application");
-        SpringApplication.run(BookshopService.class, args);
+        SpringApplication app = new SpringApplication(BookshopService.class);
+        app.setBannerMode(Banner.Mode.OFF); // Turn off the banner
+        app.setLogStartupInfo(false); // stops default log info
+        app.run(args);
         System.out.println("Startup complete");
         System.out.println("\thttp://localhost:8080/service/book/list");
         System.out.println("\thttp://localhost:8080/service/book/123");
