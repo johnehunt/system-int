c8d4d721f188044a78c4babcbef633a45e4a1481
John Hunt <john.hunt10@gmail.com>
2022-10-12 10:51:03 +0100

    Basic spring boot application

---
+++ 03-spring-boot-app/src/main/java/com/jjh/springbootapp/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.jjh.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
